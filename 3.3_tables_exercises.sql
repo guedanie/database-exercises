02243f9d1f96259e4a5d798bbabfe93f083a3dea
guedanie <danguerrero93@gmail.com>
2020-02-07 15:39:24 -0600

    worked on new queries, and made some changes after reviwing

--- 3.3_tables_exercises.sql
+++ 3.3_tables_exercises.sql
@@ -5,7 +5,7 @@ USE employees;
 SHOW TABLES;
 
 ## Explore the employees table to see data types
-SHOW CREATE TABLE employees;
+DESCRIBE employees;
 
 DESCRIBE employees;
 
@@ -17,13 +17,13 @@ DESCRIBE employees_with_departments;
 
 
 #Which tables comtain numeric type column?
-## All != departments
+## All except departments
 
 # Which table contains a string type column?
-## All != salaries
+## All except salaries
 
 # Which table contains a data? 
-## ALl != departments & employees_with_departments
+## ALl except departments & employees_with_departments
 
 #What is the relationship between the employees and the departments table?
 # There is no relationship between the two tables. For us to relate these two data sets, we would need the "employees_with_departments" table
@@ -42,11 +42,19 @@ SELECT count(*), gender FROM employees GROUP BY gender;
 #What is the average salary for all employees
 SELECT count(emp_no), sum(salary), AVG(salary) FROM salaries;
 
-#What is the average salary by gender
+#What is the average salary by gender - PROBLEM there duplicates in the salaries.empo_no, as they may have received a promotion, which could be affecting data
 SELECT gender, AVG(salary), count(gender) 
 	FROM employees
 	JOIN salaries
 	ON salaries.emp_no = employees.emp_no
+	GROUP BY gender; 
+	
+# Trying to fix the duplicate risk by looking at most recent dates
+SELECT gender, AVG(salary), count(gender) 
+	FROM employees
+	JOIN salaries
+	ON salaries.emp_no = employees.emp_no
+	WHERE salaries.to_date > 2000-01-01
 	GROUP BY gender; 
 
 #What is the gender distribution by gender?
