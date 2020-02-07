9d9c582f7b149c1313efd769f288c1ac9c2a3686
guedanie <danguerrero93@gmail.com>
2020-02-07 16:33:00 -0600

    Made changes to the comments and cleaned the select tables to be easier to read

--- 3.4_select_exercises.sql
+++ 3.4_select_exercises.sql
@@ -10,7 +10,7 @@ SELECT NAME FROM albums
 WHERE artist = "Pink Floyd";
 
 #The year Sgt. Pepper's Lonely Hearts Club band was released
-SELECT release_date FROM albums
+SELECT release_date, NAME, artist FROM albums
 WHERE NAME = "Sgt. Pepper's Lonely Hearts Club Band";
 
 #The genre for album Nevermind
@@ -18,14 +18,16 @@ SELECT genre FROM albums
 WHERE NAME = "Nevermind";
 
 #Which albums were released in the 1990's
-SELECT NAME FROM albums
-WHERE release_date BETWEEN "1990" AND "1999";
+SELECT NAME, release_date FROM albums
+WHERE release_date BETWEEN "1990" AND "1999"
+ORDER BY release_date;
 
 #Which albums had less than 20 million certified sales
-SELECT NAME FROM albums
-WHERE sales < 20;
+SELECT NAME, sales FROM albums
+WHERE sales < 20
+ORDER BY sales;
 
-#All the albums with a genre of "Rock"
-SELECT NAME FROM albums 
+#All the albums with a genre of "Rock" - it doesn't pick alternatives, like Hard Rock, because SQL is very specific, unless otherwise specified.
+SELECT NAME, genre FROM albums 
 WHERE genre = "Rock";
 
