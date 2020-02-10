75b69c88f0eacf88cd98aa88ae7b25d67682322e
guedanie <danguerrero93@gmail.com>
2020-02-10 14:35:27 -0600

    first commit using the limit exercises

--- 3.5.3_limit_excersises.sql
+++ 3.5.3_limit_excersises.sql
@@ -65,12 +65,13 @@ FROM employees
 WHERE last_name LIKE 'E%' AND last_name LIKE '%E'
 ORDER BY emp_no DESC;
 
-#Find all employees hired in the 90s and born on Christmas
+#Find all employees hired in the 90s and born on Christmas and try to find the results from page 10 - meaning offset the results by 45
 SELECT *
 FROM employees
 WHERE (hire_date BETWEEN "1990-01-01" AND "1999-12-31")
 AND birth_date LIKE "%12-25"
-ORDER BY birth_date ASC, hire_date DESC;
+ORDER BY birth_date ASC, hire_date DESC
+LIMIT 5 OFFSET 45;
 
 #Find all employees with a 'q' in their last name but not 'qu'
 SELECT *
@@ -78,5 +79,8 @@ FROM employees
 WHERE last_name LIKE '%q%'
 AND last_name NOT LIKE '%qu%';
 
-
-
+#List all unique titles within the company
+SELECT DISTINCT last_name
+FROM employees
+ORDER BY last_name DESC 
+LIMIT 10;
