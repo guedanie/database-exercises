02e3816c1daa3a58d9d6145c31fdd31a66ec4041
guedanie <danguerrero93@gmail.com>
2020-02-10 12:29:22 -0600

    added new ways to extract specific months or years from the date colunm

--- 3.5.1_where_exercises.sql
+++ 3.5.1_where_exercises.sql
@@ -22,6 +22,11 @@ SELECT *
 FROM employees
 WHERE birth_date LIKE '%12-25';
 
+#Alternative solution
+SELECT *
+FROM employees
+WHERE MONTH(birth_date) = "12" AND DAY(birth_date) = "25";
+
 #Find all employees with a 'q' last name
 SELECT *
 FROM employees
@@ -30,12 +35,15 @@ WHERE last_name LIKE '%q%';
 #Update query to use or instead of in
 SELECT *
 FROM employees
-WHERE first_name LIKE 'Irena' OR first_name LIKE 'Vidya' OR first_name LIKE 'Maya';
+WHERE first_name = 'Irena' 
+OR first_name = 'Vidya' 
+OR first_name = 'Maya';
 
 #Add condition to previous query to find male
 SELECT * 
 FROM employees
-WHERE (first_name LIKE 'Irena' OR first_name LIKE 'Vidya' OR first_name LIKE 'Maya') AND gender LIKE 'M';
+WHERE (first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya') 
+AND gender = 'M';
 
 #Find all employees whose last name starts or ends with E
 SELECT last_name
@@ -50,14 +58,16 @@ WHERE last_name LIKE 'E%' and last_name LIKE '%E';
 #Find all employees hired in the 90s and born on Christmas
 SELECT *
 FROM employees
-WHERE hire_date BETWEEN "1990-01-01" AND "1999-12-31" AND birth_date LIKE "%12-25";
+WHERE (hire_date BETWEEN "1990-01-01" AND "1999-12-31")
+AND birth_date LIKE "%12-25";
 
 #Find all employees with a 'q' in their last name but not 'qu'
 SELECT *
 FROM employees
-WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%';
+WHERE last_name LIKE '%q%'
+AND last_name NOT LIKE '%qu%';
 
 #Testing ways to extract date and month
 SELECT *
 FROM employees
-WHERE birth_date LIKE MONTH(12);
+WHERE YEAR(hire_date) < 1990;
