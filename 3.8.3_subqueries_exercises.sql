579b0b62811bcedada5475c912f2e1e553ec0c2a
guedanie <danguerrero93@gmail.com>
2020-02-12 15:33:33 -0600

    added new comments and completed the employees no longer working on the company questions

--- 3.8.3_subqueries_exercises.sql
+++ 3.8.3_subqueries_exercises.sql
@@ -28,7 +28,17 @@ GROUP BY titles.title
 ;
 
 #How many people in the employees table are no longer working for the company?
-# Skipping for now, as there is no current way to know if the employee has left the company, or simply moved departments or new salary
+SELECT Count(emp_no)
+FROM employees
+WHERE emp_no IN (
+	SELECT emp_no
+	FROM salaries
+	WHERE to_date < NOW()
+) AND emp_no IN (
+	SELECT emp_no
+	FROM dept_emp
+	WHERE to_date < NOW()
+)
 ;
 
 #Find all the current department managers that are female.
@@ -46,14 +56,27 @@ SELECT AVG(salary)
 FROM salaries
 ;
 
+#Example query
+SELECT title, count(*)
+FROM titles
+WHERE emp_no IN (
+	SELECT emp_no
+	FROM salaries
+	WHERE salary BETWEEN 67000 AND 70000
+	AND to_date > now()
+)
+AND to_date > now()
+GROUP BY title
+;
 
+#Actual query
 SELECT employees.first_name, employees.last_name, salaries.salary
 FROM employees
 JOIN salaries ON employees.emp_no = salaries.emp_no
 WHERE salary IN (
 	SELECT salary
 	FROM salaries
-	WHERE salary > AVG(salary)
+	WHERE salary > 
 	) AND salaries.to_date > NOW()
 
 ;
