bd0588ed18f2e7f53c6bc72b51a708c2847b9ec2
guedanie <danguerrero93@gmail.com>
2020-03-02 15:08:15 -0600

    File name change

---
+++ 3.5.2_order_by_exercises.sql
@@ -0,0 +1,82 @@
+USE employees;
+
+SELECT * FROM employees;
+
+#Find all employees with first anmes "Irena, Vidya or Maya, sort by first name then last name 
+SELECT *
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+ORDER BY first_name, last_name;
+
+#Update the order clause
+SELECT *
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+ORDER BY last_name, first_name;
+
+#Order by employee number
+SELECT *
+FROM employees
+WHERE last_name LIKE "e%"
+ORDER BY emp_no;
+
+#Find all employees hired in the 90s
+SELECT *
+FROM employees
+WHERE hire_date BETWEEN "1990-01-01" AND "1999-12-31";
+
+#Find all employees born on christams
+SELECT *
+FROM employees
+WHERE birth_date LIKE '%12-25';
+
+#Alternative solution
+SELECT *
+FROM employees
+WHERE MONTH(birth_date) = "12" AND DAY(birth_date) = "25";
+
+#Find all employees with a 'q' last name
+SELECT *
+FROM employees
+WHERE last_name LIKE '%q%';
+
+#Update query to use or instead of in
+SELECT *
+FROM employees
+WHERE first_name = 'Irena' 
+OR first_name = 'Vidya' 
+OR first_name = 'Maya';
+
+#Add condition to previous query to find male
+SELECT * 
+FROM employees
+WHERE (first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya') 
+AND gender = 'M';
+
+#Order by employee number in ascending order 
+SELECT *
+FROM employees
+WHERE last_name LIKE 'E%' OR last_name LIKE '%E'
+ORDER BY emp_no ASC;
+
+#Order by employee number in descending order
+SELECT *
+FROM employees
+WHERE last_name LIKE 'E%' AND last_name LIKE '%E'
+ORDER BY emp_no DESC;
+
+#Find all employees hired in the 90s and born on Christmas
+SELECT *
+FROM employees
+WHERE (hire_date BETWEEN "1990-01-01" AND "1999-12-31")
+AND birth_date LIKE "%12-25"
+ORDER BY birth_date ASC, hire_date DESC;
+
+#Find all employees with a 'q' in their last name but not 'qu'
+SELECT *
+FROM employees
+WHERE last_name LIKE '%q%'
+AND last_name NOT LIKE '%qu%';
+
+
+
